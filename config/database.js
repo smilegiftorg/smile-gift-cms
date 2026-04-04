ff87dcb6846661ade6b8c8722c76f4bc7ddde4ec
Tavi Castro <hanminhdanh1325@gmail.com>
2026-04-04 20:11:09 +0700

    fix: fixed url db

--- config/database.js
+++ config/database.js
@@ -18,7 +18,8 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      connectionString: env("DATABASE_URL"),
+      connectionString:
+        "postgresql://postgres.gaicbawgdmknchhyvdob:NZ2YVJiaE8kREsym@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres?pgbouncer=true&sslmode=require",
       ssl: {
         rejectUnauthorized: false,
       },
