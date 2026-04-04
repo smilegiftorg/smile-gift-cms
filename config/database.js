ef1bf76d9ad3cfd32925257ed489a594fa0de379
Tavi Castro <hanminhdanh1325@gmail.com>
2026-04-04 18:31:33 +0700

    fix: fixed url db

--- config/database.js
+++ config/database.js
@@ -1,12 +1,24 @@
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       host: env("DATABASE_HOST"),
+//       port: env.int("DATABASE_PORT"),
+//       database: env("DATABASE_NAME"),
+//       user: env("DATABASE_USERNAME"),
+//       password: env("DATABASE_PASSWORD"),
+//       ssl: {
+//         rejectUnauthorized: false,
+//       },
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
+      connectionString: env("DATABASE_URL"),
       ssl: {
         rejectUnauthorized: false,
       },
