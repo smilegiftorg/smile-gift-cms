4d0b6d99d7b1229a7471cbce0b13f4e87170bd0f
Tavi Castro <hanminhdanh1325@gmail.com>
2026-04-27 18:02:43 +0700

    fix: update database connection to use environment variable

--- config/database.js
+++ config/database.js
@@ -1,15 +1,9 @@
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
-    connection: {
-      host: "aws-1-ap-southeast-1.pooler.supabase.com",
-      port: 6543,
-      database: "postgres",
-      user: "postgres.gaicbawgdmknchhyvdob",
-      password: "NZ2YVJiaE8kREsym",
-      ssl: {
-        rejectUnauthorized: false,
-      },
+    connection: env("DATABASE_URL"),
+    ssl: {
+      rejectUnauthorized: false,
     },
   },
 });
